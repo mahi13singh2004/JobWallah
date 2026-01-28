f6696eb00b66abe075f388d4e29209074694fdaa
mahi13singh2004 <mahi13singh2004@gmail.com>
2026-01-29 00:03:45 +0530

    backend auth done

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./db/connectDB.js"
+import cookieParser from "cookie-parser"
+import authRoutes from "./routes/auth.route.js"
+const app=express()
+dotenv.config()
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth",authRoutes)
+
+const PORT=process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+    connectDB()
+    console.log(`Server is running at ${PORT}`)
+})
---
+++ backend/utils/generateTokenAndSetCookie.js
@@ -0,0 +1,20 @@
+const generateTokenAndSetCookie=async(id,res)=>{
+    try {
+        const token=jwt.sign({id},process.env.JWT_SECRET,{
+            expiresIn:"7d"
+        })
+        res.cookie("token",token,{
+            httpOnly:true,
+            sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+            secure: process.env.NODE_ENV === "production",
+            maxAge:7*24*60*60*1000
+        })
+        return token
+    } 
+    catch (error) {
+        console.log("Error in generating and setting cookie",error)
+        return res.status(500).json({message:"Internal server erorr"})    
+    }
+}
+
+export default generateTokenAndSetCookie
