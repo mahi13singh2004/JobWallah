8c1c3e583509f99bc1b3ce54e927a69fdf4832ea
mahi13singh2004 <mahi13singh2004@gmail.com>
2026-01-30 19:19:16 +0530

    added backend deploy url

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const axiosInstance=axios.create({
-    baseURL:"http://localhost:5000",
+    baseURL:"https://jobwallah-backend-yo6q.onrender.com",
     withCredentials:true
 })
 export default axiosInstance
